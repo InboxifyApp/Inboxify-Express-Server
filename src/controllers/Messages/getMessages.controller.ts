a384a9c482d16d88db224573bdac34cfffdf02bb
Alaa Barka <tmakaveli643@gmail.com>
2023-05-22 14:37:02 +0100

    save the message controller

--- src/controllers/Messages/getMessages.controller.ts
+++ src/controllers/Messages/getMessages.controller.ts
@@ -1,5 +1,6 @@
 import * as Services from './../../services/messages.services'
 import * as EXPRESS from 'express'
+import * as cls from '../../services/cluster.services'
 import  JWT  from 'jsonwebtoken'
 
 const getMessages : EXPRESS.RequestHandler = async ( req, res) =>{
@@ -19,12 +20,19 @@ const getMessages : EXPRESS.RequestHandler = async ( req, res) =>{
         res.status(401).send("You're not allowed to access this route !")
         return
     }
-    const cluster : any = req.body.cluster
-    if (!cluster) {
+    const clusterId : any = req.body.cluster
+    if (!clusterId) {
         res.status(400).send("You must provide a cluster !")
         return
     }
-    
+    const cluster : any = await cls.getClus(clusterId)
+
+
+    if (!cluster) {
+        res.status(404).send(`No cluster found with ID : ${clusterId} !`)
+        return
+    }
+
     const messages = await Services.GetAll(cluster)
     if (messages.length > 0) {
         res.status(200).send(messages)
---
+++ src/controllers/Messages/saveMessage.controller.ts
@@ -0,0 +1,50 @@
+import * as Services from '../../services/messages.services'
+import * as EXPRESS from 'express'
+import Messages from '../../models/messages.entity'
+import * as cls from '../../services/cluster.services'
+import * as Format from 'date-and-time'
+const saveMSG :EXPRESS.RequestHandler = async (req, res) =>{
+
+    const API : any = String(process.env.API) 
+    const API_FRONT = req.headers.api_key
+    if (API != API_FRONT) {
+        res.status(401).send("You're not allowed to access this route !")
+        return
+    }
+    //get the cluster id 
+    const clusterId : any = req.body.cluster
+
+    if (!clusterId) {
+        res.status(400).send("You must provide a cluster !")
+        return
+    }
+    const cluster  :any = cls.getClus(clusterId)
+    if (!cluster) {
+        res.status(404).send(`No cluster found with ID : ${clusterId} !`)
+        return
+    }
+
+    const message = new Messages
+    message.clusters = clusterId
+    message.message = req.body.message 
+    message.email = req.body.email 
+    message.phone = req.body.phone
+    message.fullname = req.body.fullname
+    message.subject = req.body.subject
+    const now : any = Format.format(new Date, 'YYYY-MM-DD HH:mm:ss')
+    message.created_at = now
+    message.updated_at = now
+
+    Services.createMsg(message).then(resp =>{
+        res.status(200).send("Message saved")
+    }).catch(e=>{
+        console.log(e)
+        res.status(500).send("Server error")
+    })
+
+
+
+
+}
+
+export default saveMSG
