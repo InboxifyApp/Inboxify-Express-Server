d7f13991544822839184dbcd8cab9d94ea43763d
Alaa Barka <tmakaveli643@gmail.com>
2023-05-22 15:49:16 +0100

    fixing some bugs

--- src/controllers/Clusters/makecluster.controller.ts
+++ src/controllers/Clusters/makecluster.controller.ts
@@ -17,11 +17,12 @@ const makeCluster : EXPRESS.RequestHandler = async(req, res)=>{
         res.status(400).send("Missing some datas ! ")
     }
 
-    const token : any = req.headers.token
-    if (!token) {
+    const key : any = req.headers.authorization
+    if (!key || key.split(" ").length != 2) {
         res.status(401).send("You're not allowed to access this route !")
         return
     }
+    const [bearer, token] = key.split(" ")
     const decoded : any = JWT.verify(token, String(process.env.JWT_SECRET))
     if (!decoded) {
         res.status(401).send("You're not allowed to access this route !")
