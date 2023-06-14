2e7d74a29c327397449e7f9451bdf271ebe48511
Alaa Barka <tmakaveli643@gmail.com>
2023-06-15 00:21:31 +0100

    receiving bearer and token

--- src/controllers/Clusters/getClusters.ts
+++ src/controllers/Clusters/getClusters.ts
@@ -11,11 +11,12 @@ const getClusters : EXPRESS.RequestHandler = async (req, res) =>{
         return
     }
     //suppoosing im gettting a token
-    const token : any = req.headers.token
-    if (!token) {
+    const key : any = req.headers.authorization
+    if (!key || key.split(" ").length != 2) {
         res.status(401).send("You're not allowed to access this route !")
         return
     }
+    const [bearer, token] = key.split(" ")
     const decoded : any = JWT.verify(token, String(process.env.JWT_SECRET))
     if (!decoded) {
         res.status(401).send("You're not allowed to access this route !")
